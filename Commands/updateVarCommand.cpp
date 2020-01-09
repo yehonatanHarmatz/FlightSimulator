325a0047a0da3c451bda22dc4b0675e7fa3551b4
Oren-Schwartz <58700758+Oren-Schwartz@users.noreply.github.com>
2020-01-09 17:52:10 +0200

    Update updateVarCommand.cpp

--- Commands/updateVarCommand.cpp
+++ Commands/updateVarCommand.cpp
@@ -4,7 +4,11 @@
 
 #include "updateVarCommand.h"
 #include "../Expression/Interpreter.h"
-
+/**
+ * The function updates the variable.
+ * @param index - the position in the lexer's vector.
+ * @return - the number of params.
+ */
 int updateVarCommand::execute(int index) {
     Interpreter i1 = Interpreter(this->st);
     float newVal = i1.interpret(getStringInVector(index + 1))->calculate();
