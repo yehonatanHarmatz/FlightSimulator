643881df128e4696472b8183651affe955fa580b
Oren-Schwartz <58700758+Oren-Schwartz@users.noreply.github.com>
2020-01-09 17:54:24 +0200

    Update whileCommand.h

--- Commands/whileCommand.h
+++ Commands/whileCommand.h
@@ -8,6 +8,12 @@
 
 class whileCommand : public conditionParser {
 public:
+    /**
+     * Constructor.
+     * @param lexerParams - the output vector of the lexer.
+     * @param st1 - a pointer the the symbol table.
+     * @param p1 - a pointer to the parser.
+     */
     whileCommand(vector<string> *lexerParams, SymbolTable *st1, Parser *p1) : conditionParser(lexerParams, st1, p1) {}
     int execute(int index) {
         this->getCommands(index);
